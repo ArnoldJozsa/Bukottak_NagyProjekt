6f7c4ac54413274578c88add950570735873a7e4
Encs2 <enikokovacs20040410@gmail.com>
2024-11-06 14:58:35 +0100

    Bejelentkezés, regisztráció
     Bejelentkezési adatokat ellenőrzi az adatbázisban, és sikeres bejelentkezés esetén átirányítja a felhasználót a főformra, míg ha a bejelentkezés nem sikeres, hibát jelez

--- CoffeShop/src/coffeeShopManagement/FXMLDocumentController.java
+++ CoffeShop/src/coffeeShopManagement/FXMLDocumentController.java
@@ -126,3 +126,131 @@ public class FXMLDocumentController implements Initializable {
             String selctData = "SELECT username, password FROM employee WHERE username = ? and password = ?";
 
             connect = database.connectDB();
+            try {
+
+                prepare = connect.prepareStatement(selctData);
+                prepare.setString(1, si_username.getText());
+                prepare.setString(2, si_password.getText());
+
+                result = prepare.executeQuery();
+
+                if (result.next()) {
+
+                    data.username = si_username.getText();
+
+                    alert = new Alert(AlertType.INFORMATION);
+                    alert.setTitle("Information Message");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Successfull Login!");
+                    alert.showAndWait();
+
+
+                    Parent root = FXMLLoader.load(getClass().getResource("mainForm.fxml"));
+
+                    Stage stage = new Stage();
+                    Scene scene = new Scene(root);
+
+                    stage.setTitle("Cafe Shop Management System");
+                    stage.setMinWidth(1100);
+                    stage.setMinHeight(600);
+
+                    stage.setScene(scene);
+                    stage.show();
+
+                    si_loginBtn.getScene().getWindow().hide();
+
+                } else {
+                    alert = new Alert(AlertType.ERROR);
+                    alert.setTitle("Error Message");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Incorrect Username/Password");
+                    alert.showAndWait();
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+
+    }
+
+    public void regBtn() {
+
+        if (su_username.getText().isEmpty() || su_password.getText().isEmpty()
+                || su_question.getSelectionModel().getSelectedItem() == null
+                || su_answer.getText().isEmpty()) {
+            alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Error Message");
+            alert.setHeaderText(null);
+            alert.setContentText("Please fill all blank fields");
+            alert.showAndWait();
+        } else {
+
+            String regData = "INSERT INTO employee (username, password, question, answer, date) "
+                    + "VALUES(?,?,?,?,?)";
+            connect = database.connectDB();
+
+            try {
+
+                String checkUsername = "SELECT username FROM employee WHERE username = '"
+                        + su_username.getText() + "'";
+
+                prepare = connect.prepareStatement(checkUsername);
+                result = prepare.executeQuery();
+
+                if (result.next()) {
+                    alert = new Alert(AlertType.ERROR);
+                    alert.setTitle("Error Message");
+                    alert.setHeaderText(null);
+                    alert.setContentText(su_username.getText() + " is already taken");
+                    alert.showAndWait();
+                } else if (su_password.getText().length() < 8) {
+                    alert = new Alert(AlertType.ERROR);
+                    alert.setTitle("Error Message");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Invalid Password, atleast 8 characters are needed");
+                    alert.showAndWait();
+                } else {
+                    prepare = connect.prepareStatement(regData);
+                    prepare.setString(1, su_username.getText());
+                    prepare.setString(2, su_password.getText());
+                    prepare.setString(3, (String) su_question.getSelectionModel().getSelectedItem());
+                    prepare.setString(4, su_answer.getText());
+
+                    Date date = new Date();
+                    java.sql.Date sqlDate = new java.sql.Date(date.getTime());
+                    prepare.setString(5, String.valueOf(sqlDate));
+                    prepare.executeUpdate();
+
+                    alert = new Alert(AlertType.INFORMATION);
+                    alert.setTitle("Information Message");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Successfully registered Account!");
+                    alert.showAndWait();
+
+                    su_username.setText("");
+                    su_password.setText("");
+                    su_question.getSelectionModel().clearSelection();
+                    su_answer.setText("");
+
+                    TranslateTransition slider = new TranslateTransition();
+
+                    slider.setNode(side_form);
+                    slider.setToX(0);
+                    slider.setDuration(Duration.seconds(.5));
+
+                    slider.setOnFinished((ActionEvent e) -> {
+                        side_alreadyHave.setVisible(false);
+                        side_CreateBtn.setVisible(true);
+                    });
+
+                    slider.play();
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
